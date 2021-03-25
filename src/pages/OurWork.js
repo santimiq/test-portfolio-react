7bda190ae642756efc150e397dd48dcf6996d8e0
Santiago Miqueo <smiqueo@hotmail.com>
2021-03-25 11:56:38 -0300

    OurWork

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,11 +1,60 @@
 import React from 'react'
+import styled from 'styled-components'
+import athlete from '../img/athlete-small.png'
+import theracer from '../img/theracer-small.png'
+import goodtimes from '../img/goodtimes-small.png'
+import {Link } from "react-router-dom"
 
 const OurWork =() => {
     return (
-        <div>
-            <h1>Work</h1>
-        </div>
+        <StyledWork>
+            <StyledMovie>
+                <h2>The Athlete</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={athlete} alt="athlete"/>
+                </Link>
+            </StyledMovie>
+            <StyledMovie>
+                <h2>The Racer</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={theracer} alt="theracer"/>
+                </Link>
+            </StyledMovie>
+            <StyledMovie>
+                <h2>Goodtimes</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={goodtimes} alt="goodtimes"/>
+                </Link>
+            </StyledMovie>
+        </StyledWork>
     )
 }
+
+const StyledWork = styled.div`
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+    background: white;
+    h2{
+        padding: 1rem 0rem;
+    }
+`;
+
+const StyledMovie = styled.div`
+    padding-bottom: 10rem;
+    .line {
+        height: 0.5rem;
+        background: #ccc;
+        margin-bottom: 3rem;
+    }
+    img{
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
+`;
 
 export default OurWork
