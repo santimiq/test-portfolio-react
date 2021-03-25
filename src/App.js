d6d17e4683c928fad4bd08c4ec8f1fefc325c87a
Santiago Miqueo <smiqueo@hotmail.com>
2021-03-25 11:26:09 -0300

    React Router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from "react"
 import GlobalStyle from "./components/GlobalStyle"
 //Import Pages
 import AboutUs from './pages/AboutUs'
+import Contact from './pages/Contact'
+import OurWork from './pages/OurWork'
 import Nav from './components/Nav'
+// Router
+import {Route, Switch} from 'react-router-dom'
 
 
 function App() {
@@ -11,7 +15,17 @@ function App() {
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs /> 
+        </Route>
+        <Route path="/work">
+         <OurWork />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
     </div>
   );
 }
